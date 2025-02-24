b18d1c6300b60535b991b26909be93568174fd0a
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-02-24 02:46:08 +0100

    uat config, and GHA build

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  backend: {
+    api: {
+      host: 'https://api.smart-scrum-poker.uat.balazskrizsan.com/'
+    },
+    wss_api: {
+      host: 'wss://api.smart-scrum-poker.uat.balazskrizsan.com/ws'
+    }
+  }
+};
