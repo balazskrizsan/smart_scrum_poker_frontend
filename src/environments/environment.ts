2af7ad7abcca5fef0793d4ca2101db6ede4aca9d
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-02-28 20:18:48 +0100

    default env fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   backend: {
     api: {
-      host: 'https://localhost:47050/'
+      host: 'https://localhost.balazskrizsan.com:46031/'
     },
     wss_api: {
-      host: 'wss://localhost:47050/ws'
+      host: 'wss:///localhost.balazskrizsan.com:46031/ws'
     }
   }
 };
