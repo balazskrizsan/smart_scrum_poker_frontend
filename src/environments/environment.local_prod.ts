9f7e6001c1774655abb7c25e157c8e0eab5a8bad
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-03-24 02:22:39 +0100

    Local_prod added

---
+++ src/environments/environment.local_prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  backend: {
+    api: {
+      host: 'https://api--smart-scrum-poker.localhost.balazskrizsan.com.com/'
+    },
+    wss_api: {
+      host: 'wss://api--smart-scrum-poker.localhost.balazskrizsan.com.com/ws'
+    }
+  }
+};
