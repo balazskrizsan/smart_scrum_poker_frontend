50ae498d89bca816ff28a59b3eb1aa2f8909e7b5
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-03-24 02:51:59 +0100

    balazskrizsan.com.com not good

--- src/environments/environment.local_prod.ts
+++ src/environments/environment.local_prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   backend: {
     api: {
-      host: 'https://api--smart-scrum-poker.localhost.balazskrizsan.com.com/'
+      host: 'https://api--smart-scrum-poker.localhost.balazskrizsan.com/'
     },
     wss_api: {
-      host: 'wss://api--smart-scrum-poker.localhost.balazskrizsan.com.com/ws'
+      host: 'wss://api--smart-scrum-poker.localhost.balazskrizsan.com/ws'
     }
   }
 };
