959667c6a92d80d67f01259ae17f7ed6956ff857
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-04-04 02:30:11 +0200

    UAT config update

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   backend: {
     api: {
-      host: 'https://api.smart-scrum-poker.uat.balazskrizsan.com/'
+      host: 'https://api.uat.smartscrumpoker.balazskrizsan.com/'
     },
     wss_api: {
-      host: 'wss://api.smart-scrum-poker.uat.balazskrizsan.com/ws'
+      host: 'wss://api.uat.smartscrumpoker.balazskrizsan.com/ws'
     }
   }
 };
