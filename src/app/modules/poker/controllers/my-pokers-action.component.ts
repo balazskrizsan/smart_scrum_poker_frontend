f1ab1b587df03a1fbe05765f86e8aa2ff4550442
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-02-27 22:03:22 +0100

    my-pokers listing with BehaviorSubject

--- src/app/modules/poker/controllers/my-pokers-action.component.ts
+++ src/app/modules/poker/controllers/my-pokers-action.component.ts
@@ -2,29 +2,24 @@ import {
     Component,
     OnDestroy
 }                              from '@angular/core';
-import {Forms}          from '../forms';
-import {
-    FormGroup,
-    ReactiveFormsModule
-}                       from "@angular/forms";
-import {RxStompService} from "../../commons/services/rx-stomp-service";
+import {Forms}                 from '../forms';
+import {FormGroup}             from "@angular/forms";
+import {RxStompService}        from "../../commons/services/rx-stomp-service";
 import {AccountService}        from "../../account/service/account-service";
 import {SocketDestination}     from "../../commons/enums/socket-destination";
-import {SubscriptionService}   from "../service/subscription-service";
+import {BehaviorSubject}       from "rxjs";
 import {ISubscriptionListener} from "../interfaces/i-subscription-listener";
 import {IMyPokersResponse}     from "../interfaces/i-my-pokers-response";
 import {IPoker}                from "../interfaces/i-poker";
 import {UrlService}            from "../../commons/services/url-service";
-import {
-    RouterLinkActive,
-    RouterModule
-} from "@angular/router";
+import {RouterModule}          from "@angular/router";
+import {CommonModule}           from "@angular/common";
 
 @Component(
   {
       templateUrl: '../views/my-pokers.html',
-      standalone: true,
-      imports: [RouterModule],
+      standalone:  true,
+      imports:     [RouterModule, CommonModule],
       styleUrls:   [],
       providers:   [Forms],
   }
@@ -34,13 +29,13 @@ export class MyPokersActionComponent implements OnDestroy
     protected urlService = UrlService;
     protected form: FormGroup;
     protected myPokersListener: ISubscriptionListener<IMyPokersResponse>;
-    protected myPokers: Array<IPoker> = [];
+    private myPokersSubject = new BehaviorSubject<Array<IPoker>>([]);
+    protected myPokers$ = this.myPokersSubject.asObservable();
 
     public constructor(
       protected forms: Forms,
       private rxStompService: RxStompService,
       private accountService: AccountService,
-      private subscriptionService: SubscriptionService,
     )
     {
         let user = this.accountService.getCurrentUserOrRedirect();
@@ -51,11 +46,10 @@ export class MyPokersActionComponent implements OnDestroy
         );
 
         this.myPokersListener.$subscription = this.myPokersListener.observable.subscribe(
-          (body) => this.myPokers = body.data.pokers
+          (body) => this.myPokersSubject.next(body.data.pokers)
         );
 
         this.rxStompService.publish(SocketDestination.POKER__MY_TICKETS, {userIdInsecure: user.idSecure});
-        this.subscriptionService.unsubscribe();
     }
 
     ngOnDestroy(): void
